7397f9a97d813e62045c0aaff1666deac423377d
SHIBINU001 <000928128@mohawkcollege.ca>
2024-04-03 11:00:14 -0400

    Add Cheessboard for the game
    no connection with Program cs yet, just a window with a width and height of 600 pixels and sets the title of the window to "3x3 Chessboard"  each cell is 200 pixels by 200 pixels. and cell's color alternates between yellow and white. will keep drawing chessboard unless close the window

---
+++ ConsoleApp1/ChessBoard.cs
@@ -0,0 +1,39 @@
+﻿using Raylib_cs;
+using System;
+
+namespace ChessboardExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int screenWidth = 600;
+            const int screenHeight = 600;
+            const int cellSize = 200; 
+
+            Raylib.InitWindow(screenWidth, screenHeight, "3x3 Chessboard");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose()) 
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.White);
+
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        
+                        Color color = (row + col) % 2 == 0 ? Color.Yellow : Color.White;
+                        Raylib.DrawRectangle(col * cellSize, row * cellSize, cellSize, cellSize, color);
+                    }
+                }
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
