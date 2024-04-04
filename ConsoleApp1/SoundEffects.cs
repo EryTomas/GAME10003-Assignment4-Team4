86d3a1f393e9cf1810c021e917b469daf4952d06
brad quijano <dukeisblue@outlook.com>
2024-04-04 14:37:59 -0400

    created function to unload sounds

--- ConsoleApp1/SoundEffects.cs
+++ ConsoleApp1/SoundEffects.cs
@@ -43,12 +43,14 @@ namespace ConsoleApp1
         {
             PlaySound(sounds[4]);
         }
-
-
-
         public void EndSounds()
         {
             UnloadSound(sounds[0]);
+            UnloadSound(sounds[1]);
+            UnloadSound(sounds[2]);
+            UnloadSound(sounds[3]);
+            UnloadSound(sounds[4]);
+
             CloseAudioDevice();
         }
     }
