70ace07389317b96e67d992f151c2d980fb717ac
SHIBINU001 <000928128@mohawkcollege.ca>
2024-04-04 19:23:15 -0400

    Update sound to the team's code

---
+++ ConsoleApp1/SoundEffects.cs
@@ -0,0 +1,39 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Raylib_cs.Raylib;
+
+namespace ConsoleApp1
+{
+    public class SoundEffects
+    {
+        //Hold sounds files
+        Sound[] sounds = new Sound[4];
+        public SoundEffects()
+        {
+            //Load Sounds
+            sounds[0] = LoadSound("../../../Sounds/Buzzer.mp3");
+            sounds[1] = LoadSound("../../../Sounds/Boom.mp3");
+            sounds[2] = LoadSound("../../../Sounds/Ping.mp3");
+            sounds[3] = LoadSound("../../../Sounds/Pong.mp3");
+            sounds[4] = LoadSound("../../../Sounds/Bell.mp3");
+          
+
+
+        }
+
+        public void PlayBuzzerSound()
+        {
+            PlaySound(sounds[0]);
+        }
+
+        public void EndSounds()
+        {
+            UnloadSound(sounds[0]);
+            CloseAudioDevice();
+        }
+    }
+}
