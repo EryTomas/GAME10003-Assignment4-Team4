de7837f19f6b2e310544bf76adc4da549d42bef8
brad quijano <dukeisblue@outlook.com>
2024-04-04 16:12:04 -0400

    loaded a Boom sound effect

--- ConsoleApp1/SoundEffects.cs
+++ ConsoleApp1/SoundEffects.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Raylib_cs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Raylib_cs.Raylib;
 
 namespace ConsoleApp1
 {
-    internal class SoundEffects
+    public class SoundEffects
     {
+        //Hold sounds files
+        Sound[] sounds = new Sound[4];
+        public SoundEffects()
+        {
+            //Load Sounds
+            sounds[0] = LoadSound("../../../Sounds/Buzzer.mp3");
+            sounds[1] = LoadSound("../../../Sounds/Boom.mp3");
 
-    }
+        }
 }
